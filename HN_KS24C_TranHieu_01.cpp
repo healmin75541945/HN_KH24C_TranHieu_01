6fe52346d70f795c85712da293bf8e9a09e4ab15
healmin75541945 <healminxinhgai@gmail.com>
2024-12-05 11:56:07 +0700

    Add files via upload

---
+++ HN_KS24C_TranHieu_01.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+ int main(){
+ 
+ pritf 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+pritf(\n  MENU       pintf( Nhap so vao gia tri cua tung mang)
+pritf 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+      case 3 ( //Dem so luong hoan hao trong cac mang  )
+       for (int)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 	
+ 	
+ 	
+ }
